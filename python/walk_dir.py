4a26c8435ab5a199e22e71025fad74f5296946ba
denny <wanziforever@gmail.com>
2014-05-08 10:43:06 +0800

    add a walk dir tool for matching file and some operation

---
+++ python/walk_dir.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import re
+
+def match_file(path):
+    d = os.path.dirname(path)
+    f = os.path.basename(path)
+    #--------------------------------------------------
+    # some work for match file for special requirement
+    #--------------------------------------------------
+    if os.path.islink(path):
+        return True
+    return False
+
+def op_file(path):
+    print path
+        
+if __name__ == "__main__":
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    for p, d ,files in os.walk(pwd):
+        for f in files:
+            path = os.path.join(p, f)
+            if not match_file(path):
+                continue
+            op_file(path)
+
+            
